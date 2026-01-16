2e21b0002ccc723895cf4b3420f0c6558afe1a39
Pratiksha Wanjari <127431343+pwanjari23@users.noreply.github.com>
2026-01-16 14:50:09 +0530

    Task for routers and filters completed

--- routers_filters.js
+++ routers_filters.js
@@ -3,10 +3,12 @@ const app = express();
 const PORT = 3000;
 const orderRouter = require("./routes/orders");
 const userRouter = require("./routes/users");
+const bookRouter = require("./routes/books");
 
 //integrating router
 app.use("/orders", orderRouter);
 app.use("/users", userRouter);
+app.use("/books", bookRouter);
 
 app.use(express.json());
 
